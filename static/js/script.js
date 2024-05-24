687e4dfe2efd77cb7b05ff223a8ae32dda673c52
João <86298585+jaolvz@users.noreply.github.com>
2024-05-24 02:00:53 -0300

    mudando scrollbar

--- static/js/script.js
+++ static/js/script.js
@@ -1,22 +1,21 @@
  function darkmode() {
-    // Obtém a referência do html
+  
     const html = document.documentElement;
 
-    // Alterna a classe dark-mode no html
+    // alterna o dark-mode
     html.classList.toggle('dark-mode');
 
-    // Obtém a referência da imagem
+
     const image = document.getElementById('minha_foto');
     const btn = document.getElementById('btn_dark')
-    // Verifica se está no dark mode
+
     if (html.classList.contains('dark-mode')) {
-      // Altera o src da imagem para a versão dark mode
+
       image.src = 'static/imgs/darkmode.jpg';
       btn.classList.remove('fa-moon')
-      btn.classList.add('fa-sun');
-      
-    } else {
-      // Altera o src da imagem para a versão light mode
+      btn.classList.add('fa-sun'); 
+    }
+     else {
       image.src = 'static/imgs/eunaoseiprogramar.png';
       btn.classList.remove('fa-sun')
       btn.classList.add('fa-moon');
